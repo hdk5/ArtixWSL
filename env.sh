a7952e1e8836cfb7f1a8415a82571bbe02d11ca7
hdk5 <hdk5@users.noreply.github.com>
2020-11-27 01:42:00 +0300

    Update artix iso mirror and version

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 ARCH="x86_64"
-ISO_VER="20200214"
+ISO_VER="20201109"
 ISO_ED="base"
 ISO_INIT="runit"
 ISO_FN="artix-${ISO_ED}-${ISO_INIT}-${ISO_VER}-${ARCH}.iso"
-ISO_URL="https://download.artixlinux.org/iso/${ISO_FN}"
+ISO_URL="https://iso.artixlinux.org/iso/${ISO_FN}"
 
 FRTCP_BLD="18082100"
 FRTCP_VER="1.23-1"
